aed69359df947a822ecdaaaca9e5af3ca77f47a1
tonyfran94 <44416292+tonyfran94@users.noreply.github.com>
2025-11-08 09:44:59 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
